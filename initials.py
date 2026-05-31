250cc7e4b6339dd2dc2798dd915edd4d3a0fcb98
Akli-ux <2425403526@live.gctu.edu.gh>
2026-05-31 03:08:31 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
 
+# Name: Philip Akli Gbeku
+# STUDENT ID: 2425403526
+# Email: 2425403526@live.gctu.edu.gh
+# Grader: Augustus Buckman
+
+# On my honor,Philip Akli Gbeku,this programming assignment is my own work
+# and I have not provided this code to any other student.
 def main():
   pass #code goes here
 
-
+ # A python code to print out my initials: P.A.G
+    print()
+    print("...PAG")
+    print()
+    print("...PPPPPPPPPPP.............AAAA.............GGGGGGG.........")
+    print("...P..........P...........A....A...........G................")
+    print("...P..........P..........A......A..........G................")
+    print("...P..........P.........A........A.........G................")
+    print("...PPPPPPPPPPP.........AAAAAAAAAAAA........G................")
+    print("...P...................A..........A........G....GGGG........")
+    print("...P...................A..........A........G....GGGG........")
+    print("...P...................A..........A........G....GGGG........")
+    print("...P.............**....A..........A...**...G....GGGG......**")
+    print("...P.............**....A..........A...**....GGGGGGGG......**")
+    print()
 
 main()
